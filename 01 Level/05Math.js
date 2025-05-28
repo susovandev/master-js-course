2173b8f5d38d26595ccf741d053814b47f5b20dd
Susovan Dev <susovandas.dev@gmail.com>
2025-05-28 12:05:52 +0530

    Lecture: Math Object

---
+++ 01 Level/05Math.js
@@ -0,0 +1,37 @@
+/*
+    Math.round()
+    Math.floor()
+    Math.ceil()
+    Math.trunc()
+    Math.PI()
+    Math.Pow()
+    Math.sqrt()
+    Math.abs()
+    Math.random()
+*/
+
+console.log(Math.round(3.4));
+console.log(Math.round(3.6));
+
+console.log(Math.floor(3.4));
+console.log(Math.floor(3.6));
+
+console.log(Math.ceil(3.4));
+console.log(Math.ceil(3.6));
+
+console.log(Math.trunc(3.4));
+console.log(Math.trunc(3.6));
+
+console.log(Math.PI.toFixed(3));
+
+console.log(Math.sqrt(36))
+
+console.log(Math.pow(2, 3))
+console.log(Math.abs(-4))
+
+
+// Math.random()
+
+console.log(Math.floor(Math.random() * 900000 + 100000))
+
+// Math.random() --> 0 - 1 (1 * 9999 + 1000) = 5 digits
